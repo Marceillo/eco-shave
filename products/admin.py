0ed5d08cc8c312607d661954d6d6a30a9849e018
Marceillo De Wet <marceillo@hotmail.com>
2024-10-16 17:55:09 +0000

    Add intial admin view and included previewimage and rating

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Category, PreviewImage
+from star_ratings.models import Rating
 
 # Register your models here.
+class PreviewImageInline(admin.TabularInline):
+    model = PreviewImage
+    extra = 1 
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        # 'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+    inlines = [PreviewImageInline]
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
