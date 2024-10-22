8f09e63512df8d29874680e1a34c5945c119a26b
Marceillo De Wet <marceillo@hotmail.com>
2024-10-22 11:22:15 +0000

    Update bag template and add feat add and view  initial code for view/bag

--- bag/views.py
+++ bag/views.py
@@ -8,15 +8,31 @@ from products.models import Product
 def view_bag(request):
     """ A view that renders the bag contents page """
 
-    return render(request, 'bag/bag.html')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    context = {
+        'items_in_bag_contents':
+        sum(shopping_bag.values()),
+        'shopping_bag': shopping_bag,
+
+    }
+
+    return render(request, 'bag/bag.html', context)
 
 
-# def add_to_bag( request, item_id):
-#     """
-#     Add products to the shopping bag. 
-#     """
+def add_to_bag( request, item_id):
+    """
+    Add products to the shopping bag. 
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    shopping_bag = request.session.get('shopping_bag', {})
 
-
+    if item_id in shopping_bag:
+        shopping_bag[item_id] += 1 
+        else:
+            shopping_bag[item_id]= 1
+        request.session['shopping_bag'] = shopping_bag
+        messages.success(request, f'Added {product.name} to your bag.') 
 
 
 
