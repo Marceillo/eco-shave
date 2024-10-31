70a75466166b3d05a0a9d5edb1e59aa525b02543
Marceillo De Wet <marceillo@hotmail.com>
2024-10-31 08:27:13 +0000

    Add stripe elements and style in css

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q3EasG1ZGJMpzARnGbA5tkDKLEwfOlpA20kPlwXt52PN660w3qYOHL6jeoZDo8GbxfwV8r8IHU8XNcL18OAbZTb00ewqNwwtf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
