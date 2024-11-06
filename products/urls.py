1febdb438105cf893356a956397fc88f66a96174
Marceillo De Wet <marceillo@hotmail.com>
2024-11-06 15:14:43 +0000

    Feat: Add the ability to add,update,delete for staff

--- products/urls.py
+++ products/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
     path('upload-product/', upload_product_view, name='upload_product'),
-    
-    # path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    # path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
       
 ]
