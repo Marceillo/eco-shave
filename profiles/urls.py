2397e1524285077711e04b5b5b6db95446278e5a
Marceillo De Wet <marceillo@hotmail.com>
2024-11-07 16:43:48 +0000

    setup wish list initial code

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     # path('edit_profile/',views.edit_profile, name='edit_profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('wishlist/', views.wishlist, name='wishlist'),
 ]
