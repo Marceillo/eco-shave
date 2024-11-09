58dd17beab3afbf8953b8b97bd1dddba0e281e55
Marceillo De Wet <marceillo@hotmail.com>
2024-11-09 13:58:20 +0000

    Linked up the about app

--- about/forms.py
+++ about/forms.py
@@ -1,8 +1,1 @@
-from django import forms
-from .models import ContactInquiry
 
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = ContactInquiry
-        fields = ['name', 'email', 'message']
--- about/views.py
+++ about/views.py
@@ -1,41 +1,15 @@
 from django.shortcuts import render, redirect
-from .forms import ContactForm
+# from .forms import ContactForm
 from django.contrib import messages
 
 
 def about(request):
     """
-    Returns the about kayaking page and
+    Returns the about page and
     display relevant information.
     """
 
     return render(request, 'about/about.html')
 
 
-def contact(request):
-    """
-    Allows a user to get in touch with the blog.
-    Using a form the user send a message with their details.
-    about/contact_kayaker.html template used.
-    """
-    form = ContactForm()
 
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request,
-                'Your message has been received successfully!')
-
-            return redirect('about_kayaking')
-        else:
-            messages.error(request, 'Error submitting your message.')
-            # form = ContactForm()
-
-    return render(request, 'about/contact_kayaker.html', {'form': form})
-
-
-def contact_success(request):
-    return render(request, 'about/contact.html')
